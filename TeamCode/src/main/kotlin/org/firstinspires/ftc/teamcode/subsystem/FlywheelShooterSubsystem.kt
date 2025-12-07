38fac83ff6b53d1b1d4a8e50b3138b89bea362ac
Josh Pavlovich <joshpavlovich@gmail.com>
2025-12-07 11:37:51 -0600

    feat: Add launch zone power constants and automate transfer
    This commit introduces two new constants for setting flywheel power based on the launch zone: `FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE` and `FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE`.
    
    The transfer commands (`transfer` and `stopTransfer`) are now private and are automatically called with the `spin` and `stopSpin` commands respectively. The transfer action is initiated after a 1.5-second delay when spinning the flywheel to allow it to reach the desired speed.
    
    Additionally, the `flyWheelMotorRight` has been reversed.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystem/FlywheelShooterSubsystem.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/subsystem/FlywheelShooterSubsystem.kt
@@ -9,13 +9,16 @@ import dev.nextftc.hardware.impl.ServoEx
 import dev.nextftc.hardware.powerable.SetPower
 import org.firstinspires.ftc.robotcore.external.Telemetry
 
+const val FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE = 0.65
+const val FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE = 0.80
+
 private const val KICKER_SERVO_DOWN_POSITION = 0.0
 private const val KICKER_SERVO_UP_POSITION = 0.35
 
 object FlywheelShooterSubsystem : Subsystem {
 
     private val flyWheelMotorLeft by lazy { MotorEx("flywheel_motor_left") }
-    private val flyWheelMotorRight by lazy { MotorEx("flywheel_motor_right") }
+    private val flyWheelMotorRight by lazy { MotorEx("flywheel_motor_right").reversed() }
 
     private val kickerServo by lazy { ServoEx("kicker_servo") }
 
@@ -29,11 +32,12 @@ object FlywheelShooterSubsystem : Subsystem {
         flyWheelMotorRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
 
         // TODO: INITIALIZE KICKER SERVO POSITION??? 0.0 or the kicker servo's down position???
-        kickerServo.position = KICKER_SERVO_DOWN_POSITION
+//        kickerServo.position = KICKER_SERVO_DOWN_POSITION
     }
 
     fun kickArtifact() = InstantCommand {
-        if (kickerServo.servo.position == KICKER_SERVO_DOWN_POSITION) kickerServo.position = KICKER_SERVO_UP_POSITION
+        if (kickerServo.servo.position == KICKER_SERVO_DOWN_POSITION) kickerServo.position =
+            KICKER_SERVO_UP_POSITION
     }.requires(this)
 
     fun resetKickerServo() = InstantCommand {
@@ -43,21 +47,25 @@ object FlywheelShooterSubsystem : Subsystem {
     // TODO: DO THE SERVOS NEED TO BE DELAYED, SO THAT THE FLYWHEEL CAN SPIN TO THE DESIRED VELOCITY
     // FIRST IF THEY ARE IN A PARALLEL GROUP? SEE, https://nextftc.dev/nextftc/commands/delays
     fun spin(power: Double = 1.0) = SetPower(flyWheelMotorLeft, power).requires(this).and(
-        SetPower(flyWheelMotorRight, power).requires(this)
+        SetPower(flyWheelMotorRight, power).requires(this),
+        // TODO: CHANGE WHEN WE KNOW HOW LONG IT TAKES TO SPIN THE FLYWHEEL
+        transfer().afterTime(1.5)
+        // TODO: CHANGE WHEN WE KNOW HOW LONG IT TAKES TO SPIN THE FLYWHEEL
     )
 
     fun stopSpin() = SetPower(flyWheelMotorLeft, 0.0).requires(this).and(
-        SetPower(flyWheelMotorRight, 0.0).requires(this)
+        SetPower(flyWheelMotorRight, 0.0).requires(this),
+        stopTransfer()
     )
 
     // TODO: CHANGE MOTOR DIRECTION (-1.0 POWER TO REVERSE) ACCORDING TO LOCATION OF SERVO (RIGHT OR LEFT AND UP OR DOWN)
-    fun transfer() = SetPower(transferServoBottomLeft, 1.0).requires(this).and(
+    private fun transfer() = SetPower(transferServoBottomLeft, 1.0).requires(this).and(
         SetPower(transferServoBottomRight, -1.0).requires(this),
         SetPower(transferServoTopLeft, 1.0).requires(this),
         SetPower(transferServoTopRight, -1.0).requires(this)
     )
 
-    fun stopTransfer() = SetPower(transferServoBottomLeft, 0.0).requires(this).and(
+    private fun stopTransfer() = SetPower(transferServoBottomLeft, 0.0).requires(this).and(
         SetPower(transferServoBottomRight, 0.0).requires(this),
         SetPower(transferServoTopLeft, 0.0).requires(this),
         SetPower(transferServoTopRight, 0.0).requires(this)
