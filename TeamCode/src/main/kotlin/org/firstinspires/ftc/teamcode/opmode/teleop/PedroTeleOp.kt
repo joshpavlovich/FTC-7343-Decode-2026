3d77c03f048c6d67c8db87416e23c04d3dfa627b
Josh Pavlovich <joshpavlovich@gmail.com>
2025-12-07 11:45:11 -0600

    Refactor TeleOp to use FlywheelShooterSubsystem
    This commit refactors `PedroTeleOp` to integrate the `FlywheelShooterSubsystem`, replacing the previous `IntakeSubsystem` logic.
    
    New controls are mapped on gamepad 1:
    -   Right trigger now controls `kickArtifact()` and `resetKickerServo()`.
    -   Circle button toggles the flywheel spin for the front launch zone.
    -   Square button toggles the flywheel spin for the back launch zone.
    
    Telemetry for the shooter has also been added to the OpMode's update loop.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/teleop/PedroTeleOp.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/teleop/PedroTeleOp.kt
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.opmode.teleop
 import com.bylazar.telemetry.JoinedTelemetry
 import com.bylazar.telemetry.PanelsTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.util.RobotLog
 import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.extensions.pedro.PedroComponent
@@ -15,13 +14,18 @@ import dev.nextftc.ftc.components.BulkReadComponent
 import org.firstinspires.ftc.teamcode.opmode.autonomous.AutonomousStateManager
 import org.firstinspires.ftc.teamcode.panels.Drawing
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants
-import org.firstinspires.ftc.teamcode.subsystem.IntakeSubsystem
+import org.firstinspires.ftc.teamcode.subsystem.FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE
+import org.firstinspires.ftc.teamcode.subsystem.FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE
+import org.firstinspires.ftc.teamcode.subsystem.FlywheelShooterSubsystem
+import org.firstinspires.ftc.teamcode.subsystem.FlywheelShooterSubsystem.addShooterDetails
+
+private const val RIGHT_TRIGGER_MINIMUM_VALUE = 0.3
 
 @TeleOp(name = "Pedro TeleOp")
 class PedroTeleOp : NextFTCOpMode() {
     init {
         addComponents(
-            SubsystemComponent(IntakeSubsystem),
+            SubsystemComponent(FlywheelShooterSubsystem),
             PedroComponent(Constants::createFollower),
             BulkReadComponent,
             BindingsComponent
@@ -43,12 +47,20 @@ class PedroTeleOp : NextFTCOpMode() {
         val driverControlled = PedroDriverControlled(
             drivePower = -Gamepads.gamepad1.leftStickY,
             strafePower = -Gamepads.gamepad1.leftStickX,
-            turnPower = -Gamepads.gamepad1.rightStickX
+            turnPower = -Gamepads.gamepad1.rightStickX,
+            robotCentric = false // TODO: WE NEED TO TEST FIELD CENTRIC VS ROBOT CENTRIC, SEE https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html#field-centric
         )
         driverControlled()
 
-        Gamepads.gamepad1.rightBumper whenBecomesTrue IntakeSubsystem.reverse whenBecomesFalse IntakeSubsystem.stop
-        Gamepads.gamepad1.leftBumper whenBecomesTrue IntakeSubsystem.forward whenBecomesFalse IntakeSubsystem.stop
+        Gamepads.gamepad1.rightTrigger.atLeast(RIGHT_TRIGGER_MINIMUM_VALUE)
+            .whenBecomesTrue(FlywheelShooterSubsystem.kickArtifact())
+            .whenBecomesFalse(FlywheelShooterSubsystem.resetKickerServo())
+        Gamepads.gamepad1.circle.toggleOnBecomesTrue().whenBecomesTrue(
+            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE)
+        ) whenBecomesFalse FlywheelShooterSubsystem.stopSpin()
+        Gamepads.gamepad1.square.toggleOnBecomesTrue().whenBecomesTrue(
+            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE)
+        ).whenBecomesFalse(FlywheelShooterSubsystem.stopSpin())
     }
 
     override fun onUpdate() {
@@ -56,8 +68,9 @@ class PedroTeleOp : NextFTCOpMode() {
         ActiveOpMode.telemetry.addData("Current pose", PedroComponent.follower.pose)
         ActiveOpMode.telemetry.addData("isBusy", PedroComponent.follower.isBusy)
 
-        RobotLog.d("Motor Voltage: Drivetrain: " + PedroComponent.follower.drivetrain.voltage.toString())
+//        RobotLog.d("Motor Voltage: Drivetrain: " + PedroComponent.follower.drivetrain.voltage.toString())
 
+        ActiveOpMode.telemetry.addShooterDetails()
         ActiveOpMode.telemetry.update()
     }
 }
