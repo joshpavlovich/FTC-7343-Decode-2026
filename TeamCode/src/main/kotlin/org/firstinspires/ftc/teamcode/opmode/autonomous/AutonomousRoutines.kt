68a4e08ba638ed5adb0255184fa8322bd5870b44
Josh Pavlovich <joshpavlovich@gmail.com>
2025-12-13 22:30:14 -0600

    Refactor: Decouple flywheel spin from path following in auto
    This commit refactors the autonomous routines to run the flywheel spin-up command sequentially after the path-following command, rather than in parallel.
    
    Key changes in `frontLaunchZoneShootingAutoRoutine` and `backLaunchMidShootingAutoRoutine`:
    - The `ParallelGroup` wrapping the `spin` and `FollowPath` commands has been removed. The flywheel now starts spinning up after the robot reaches the shooting location.
    - A `transfer()` command has been added to move an artifact into the kicker after arriving at the shooting location.
    - The sequence for shooting artifacts has been updated. It now includes explicit `resetKickerServo()` commands between kicks.
    - Delays have been adjusted to accommodate the new sequential actions.
    - A fourth `kickArtifact()` call has been added to each routine.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousRoutines.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousRoutines.kt
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous
 
 import dev.nextftc.core.commands.delays.Delay
-import dev.nextftc.core.commands.groups.ParallelGroup
 import dev.nextftc.core.commands.groups.SequentialGroup
 import dev.nextftc.extensions.pedro.FollowPath
 import org.firstinspires.ftc.teamcode.subsystem.FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_MID_AUTO_ZONE
@@ -17,16 +16,24 @@ object AutonomousRoutines {
         get() = SequentialGroup(
             // Starting at the front launch zone going to the front launch zone shooting and
             // starting flywheel motor leading into shooting
-            ParallelGroup(
-                FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_FRONT_AUTO_LAUNCH_ZONE),
-                FollowPath(PathManager.frontLaunchZoneStartToFrontLaunchZoneShooting, true)
-            ),
-            Delay(5.0),
+            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_FRONT_AUTO_LAUNCH_ZONE),
+            FollowPath(PathManager.frontLaunchZoneStartToFrontLaunchZoneShooting, true),
+            Delay(1.0),
+            FlywheelShooterSubsystem.transfer(),
+            Delay(4.0),
             FlywheelShooterSubsystem.kickArtifact(),
-            Delay(2.0),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
+            Delay(3.0),
             FlywheelShooterSubsystem.kickArtifact(),
-            Delay(2.0),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
+            Delay(3.0),
             FlywheelShooterSubsystem.kickArtifact(),
+            Delay(1.0),
+            FlywheelShooterSubsystem.kickArtifact(),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
             Delay(2.0),
             FlywheelShooterSubsystem.stopSpin(),
             // Moving to the ppg spike mark to tap artifacts into loading zone
@@ -74,16 +81,24 @@ object AutonomousRoutines {
         get() = SequentialGroup(
             // Starting at the back launch zone going to the back launch zone mid shooting and
             // starting flywheel motor leading into shooting
-            ParallelGroup(
-                FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_MID_AUTO_ZONE),
-                FollowPath(PathManager.backLaunchZoneStartToBackLaunchZoneShooting, true)
-            ),
-            Delay(5.0),
+            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_MID_AUTO_ZONE),
+            FollowPath(PathManager.backLaunchZoneStartToBackLaunchZoneShooting, true),
+            Delay(1.0),
+            FlywheelShooterSubsystem.transfer(),
+            Delay(4.0),
             FlywheelShooterSubsystem.kickArtifact(),
-            Delay(2.0),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
+            Delay(3.0),
             FlywheelShooterSubsystem.kickArtifact(),
-            Delay(2.0),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
+            Delay(3.0),
             FlywheelShooterSubsystem.kickArtifact(),
+            Delay(1.0),
+            FlywheelShooterSubsystem.kickArtifact(),
+            Delay(1.0),
+            FlywheelShooterSubsystem.resetKickerServo(),
             Delay(2.0),
             FlywheelShooterSubsystem.stopSpin(),
             // Moving to the ppg spike mark to tap artifacts into loading zone
