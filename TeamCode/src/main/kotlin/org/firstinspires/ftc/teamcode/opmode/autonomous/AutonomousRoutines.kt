85b53c9ddfa172d1136ede3fb9304653a23050fa
Josh Pavlovich <joshpavlovich@gmail.com>
2025-12-14 18:12:18 -0600

    Remove commented-out autonomous code
    This commit removes large blocks of commented-out code from the `frontLaunchZoneShootingAutoRoutine` and `backLaunchMidShootingAutoRoutine` in `AutonomousRoutines.kt`.
    
    The removed code related to paths for moving to the spike mark, loading zone, and performing additional shooting cycles.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousRoutines.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/autonomous/AutonomousRoutines.kt
@@ -36,41 +36,6 @@ object AutonomousRoutines {
             FlywheelShooterSubsystem.resetKickerServo(),
             Delay(2.0),
             FlywheelShooterSubsystem.stopSpin(),
-            // Moving to the ppg spike mark to tap artifacts into loading zone
-//            FollowPath(PathManager.frontLaunchZoneShootingToPpgSpikeMark),
-            // Moving to the loading zone with the artifacts previously tapped and get artifacts
-            // loaded into the robot than start flywheel motor leading into shooting
-//            FollowPath(PathManager.ppgSpikeMarkFromFrontToLoadingZonePark, true),
-//            Delay(3.0),
-//            ParallelGroup(
-//                FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE),
-//                // After done loading, move to the front launch zone and shoot
-//                FollowPath(PathManager.loadingZoneToFrontLaunchZoneShooting, true)
-//            ),
-//            Delay(2.5),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            FlywheelShooterSubsystem.stopSpin(),
-
-            // TODO: DO WE HAVE ENOUGH TIME TO GO BACK TO THE LOADING ZONE, SHOOT AGAIN, AND LEAVE??
-            //  IF NOT, THEN COMMENT OUT THE LINES BELOW
-//            FollowPath(PathManager.frontLaunchZoneShootingToLoadingZone, true),
-//            Delay(3.0),
-//            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_FRONT_LAUNCH_ZONE),
-//            // After done loading, move to the front launch zone and shoot
-//            FollowPath(PathManager.loadingZoneToFrontLaunchZoneShooting, true),
-//            Delay(1.5),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            FlywheelShooterSubsystem.stopSpin(),
-            // TODO: DO WE HAVE ENOUGH TIME TO GO BACK TO THE LOADING ZONE, SHOOT AGAIN, AND LEAVE??
-            //  IF NOT, THEN COMMENT OUT THE LINES ABOVE
 
             // Go from the front launch zone to outside the launch zone tape in order to get
             // leave points
@@ -101,41 +66,6 @@ object AutonomousRoutines {
             FlywheelShooterSubsystem.resetKickerServo(),
             Delay(2.0),
             FlywheelShooterSubsystem.stopSpin(),
-            // Moving to the ppg spike mark to tap artifacts into loading zone
-//            FollowPath(PathManager.backLaunchZoneShootingToPpgSpikeMark),
-            // Moving to the loading zone with the artifacts previously tapped and get artifacts
-            // loaded into the robot than start flywheel motor leading into shooting
-//            FollowPath(PathManager.ppgSpikeMarkFromBackLaunchToLoadingZonePark, true),
-//            Delay(3.0),
-//            ParallelGroup(
-//                FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE),
-//                // After done loading, move to the back launch zone and shoot
-//                FollowPath(PathManager.loadingZoneToBackLaunchZoneShooting, true)
-//            ),
-//            Delay(2.5),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            FlywheelShooterSubsystem.stopSpin(),
-
-            // TODO: DO WE HAVE ENOUGH TIME TO GO BACK TO THE LOADING ZONE, SHOOT AGAIN, AND LEAVE??
-            //  IF NOT, THEN COMMENT OUT THE LINES BELOW
-//            FollowPath(PathManager.backLaunchZoneShootingToLoadingZone, true),
-//            Delay(3.0),
-//            FlywheelShooterSubsystem.spin(FLYWHEEL_MOTOR_POWER_BACK_LAUNCH_ZONE),
-//            // After done loading, move to the back launch zone and shoot
-//            FollowPath(PathManager.loadingZoneToBackLaunchZoneShooting, true),
-//            Delay(2.5),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            Delay(1.0),
-//            FlywheelShooterSubsystem.kickArtifact(),
-//            FlywheelShooterSubsystem.stopSpin(),
-            // TODO: DO WE HAVE ENOUGH TIME TO GO BACK TO THE LOADING ZONE, SHOOT AGAIN, AND LEAVE??
-            //  IF NOT, THEN COMMENT OUT THE LINES ABOVE
 
             // Go from the back launch zone to outside the launch zone tape in order to get
             // leave points and line up robot to open the gate at start of TeleOp
